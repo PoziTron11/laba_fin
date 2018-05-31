f1555853b60043c60d27d4932ac16a23b5a47053
Mega96gost <Mega96gost@mail.ru>
2018-05-31 09:10:46 +0700

    minus5

--- laba3.1/laba3.1/MainWindow.xaml.cs
+++ laba3.1/laba3.1/MainWindow.xaml.cs
@@ -30,7 +30,9 @@ namespace lab3wpf
             res.Content = int.Parse(A.Text) + int.Parse(B.Text);
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             res.Content = int.Parse(A.Text) - int.Parse(B.Text);
         }
